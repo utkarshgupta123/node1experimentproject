827929474021fbae7b9361f9536cf1cb3cf20642
utkarsh <utkarshgupta12400@gmail.com>
2022-11-14 15:09:39 +0530

    update

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const data1 = require("./Routes/bollywood")
 
 app.use('/home',data1)
 
-app.listen(4000,()=>{
+// app.listen(4000,()=>{
+//     console.log("server running");
+// })
+
+
+
+app.listen(process.env.PORT||8080 ,()=>{
     console.log("server running");
 })
